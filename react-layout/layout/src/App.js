7a65fe7bdcb0b88c83aa30b9e47ceca106c92ead
문성호 <munsh@pdsoft.co.kr>
2021-08-05 17:26:45 +0900

    react layout update

--- react-layout/layout/src/App.js
+++ react-layout/layout/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './common/Header';
+import Footer from './common/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+const App = () => {
+    return (
+        <>
+        <Header />
+        <main>
+            <Router>
+                <Route src='/' />
+            </Router>
+        </main>
+        <Footer />
+        </>
+    )
 }
 
 export default App;
