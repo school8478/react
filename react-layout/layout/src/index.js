23f2f46d518f920abf1ca2de4157c9c30cfee823
문성호 <munsh@pdsoft.co.kr>
2021-08-05 11:30:31 +0900

    update

--- react-layout/layout/src/index.js
+++ react-layout/layout/src/index.js
@@ -1,5 +1,7 @@
-// This must be the first line in src/index.js
 import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -8,10 +10,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-<React.StrictMode>
-<App />
-</React.StrictMode>,
-document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
