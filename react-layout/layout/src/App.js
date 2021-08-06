3e178a3f0ef9d321e88db50edb8749278794b4aa
문성호 <munsh@pdsoft.co.kr>
2021-08-06 13:26:48 +0900

    layout react project

--- react-layout/layout/src/App.js
+++ react-layout/layout/src/App.js
@@ -1,21 +1,25 @@
-import Header from './common/Header';
-import Footer from './common/Footer';
+import logo from './logo.svg';
+import './App.css';
 
-import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-
-const App = () => {
+function App() {
     return (
-        <>
-        <Header />
-        <main>
-            <Router>
-                <Route src='/' />
-            </Router>
-        </main>
-        <Footer />
-        </>
-    )
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- react-layout/layout/src/index.js
+++ react-layout/layout/src/index.js
@@ -1,13 +1,22 @@
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/stable';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './custom.scss';
-
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('projectApp'));
+ReactDOM.render(
+    <React.StrictMode>
+    <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
