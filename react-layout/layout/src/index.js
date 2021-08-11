9e3d9914bbf08d6c1ede087698781602a2a1a3bb
문성호 <munsh@pdsoft.co.kr>
2021-08-11 09:38:58 +0900

    react ie update

--- react-layout/layout/src/index.js
+++ react-layout/layout/src/index.js
@@ -1,9 +1,8 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import 'core-js/features/string/repeat';
-
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
